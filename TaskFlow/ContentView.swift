23dceec21af5de4fdf8e715a127507d859d37693
Miguel Mercado <mpswift89@gmail.com>
2025-02-03 10:29:54 +0100

    testing

--- TaskFlow/ContentView.swift
+++ TaskFlow/ContentView.swift
@@ -54,6 +54,8 @@ struct ContentView: View {
             } catch {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
